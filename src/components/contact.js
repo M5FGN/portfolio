6dabe9024c2dcdec5601cbeb5b5277af7f7907bb
M5FGN <m5fgn@outlook.com>
2022-02-19 23:50:46 +0000

    Update contact and work icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,10 +9,11 @@ function Contact() {
      <h2>Contact</h2> 
      
      <ul>
-     <li><a href="mailto:m5fgn@outlook.com?subject=Message%20from%20Portfolio%20Website"><FontAwesomeIcon icon={["far", "envelope"]} /></a></li>
+     <li><a href="mailto:m5fgn@outlook.com?subject=Message%20from%20Portfolio%20Website"><FontAwesomeIcon icon={["fas", "at"]} /></a></li>
      <li><a href="https://www.linkedin.com/in/m5fgn/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
  </ul> 
  </div>
   );                          
 }                           
 export default Contact;  
+
--- src/components/mywork.js
+++ src/components/mywork.js
@@ -16,14 +16,12 @@ function MyWork() {
         <MyWorkCard3/> 
         </div>
         
-        <div className="myWork">
+        <div className="work_icons">
         <ul>
         
-     <li><a href="mailto:m5fgn@outlook.com?subject=Message%20from%20Portfolio%20Website"><FontAwesomeIcon icon={["far", "envelope"]} /></a></li>
-     <li><a href="https://www.linkedin.com/in/m5fgn/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
+        <h3 className="work_more">See more of my work ...</h3>
      <li><a href="https://github.com/M5FGN"><FontAwesomeIcon icon={['fab', 'github']} /></a></li>
      <li><a href="https://codepen.io/m5fgn"><FontAwesomeIcon icon={['fab', 'codepen']} /></a></li>
-     <li><a href="https://twitter.com/m5fgn"><FontAwesomeIcon icon={['fab', 'twitter']} /></a></li>
  </ul> 
  </div>
         
