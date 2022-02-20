8c36ccdb1341cf495d55a2e569236d2cc7aa3abe
M5FGN <m5fgn@outlook.com>
2022-02-20 12:30:20 +0000

    Added About Text

--- src/components/about.js
+++ src/components/about.js
@@ -3,9 +3,11 @@ import React from 'react';
 function About() {              
   
   return (                    
-     <div>
+     <div className="about">
       <a href="#" className="link" name="about"></a> 
       <h2>About</h2>
+<p className="about_text">      Having worked in IT Training and Support roles for may years I was made redundant in April 2020 and with the job market stagnated due the lockdown I turned to coding to pass the time. Coding was a hobby of many years which I had been lucky enough to be able to incorporate in most of my roles until then. Like many I began considering the shape of my future life during the lockdown and I decided to do more of the things I loved. Coding being one of these things. I applied and was accepted on the 16 week intensive Code Clan Software Development course and now work as a Junior Software Developer using JavaScript and SQL.</p>
+    <p className="about_text">  Outside work I am continuing to learn new coding skills particularily React and am pursuing my other love which is history. I spend my free time visiting and researching aspects of Scottish and British History and intend to start documenting this on a new blog.</p>
     </div>    
   );                          
 }                           
