526df187ecfabfb625475c752a6331e0281f0c34
M5FGN <m5fgn@outlook.com>
2022-03-05 08:32:21 +0000

    Blog details loading from API

--- src/components/blogpost1.js
+++ src/components/blogpost1.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from 'react';  
-import keyboard from '../assets/keyboard.jpg'         
+import React, {useState, useEffect} from 'react';       
 
 function BlogPost1() {  
 
@@ -12,10 +11,7 @@ function BlogPost1() {
 const fetchBlog = function() {
   const blog = fetch("https://dev.to/api/articles/1004536")
   .then((response) => response.json())
-
-  // FIXME - This is what is stopping the blog details from appearing
-  // .then((details) => {setUser(details.results[0].name.first)})
-  // .then((details) => {setBlog(details.results[0])})
+  .then((blogData) => {setBlog(blogData)})
 
   // NOTE - Include the console log to see the Promise Status in the console.
   console.log(blog);
@@ -23,11 +19,13 @@ const fetchBlog = function() {
   
   return (                    
     <div className='myblogcard card'>
-      <img className='mycardimg' src={keyboard} alt="Keyboard" /> 
-      <h3>{loadBlog.title}</h3>
-      <h4>loadBlog.readable_published_at</h4>
-      <p>loadBlog.description</p>
-      <h4>loadBlog.tag_list</h4>
+      {/* <img className='mycardimg' src={keyboard} alt="Keyboard" />  */}
+      <img className='mycardimg' src={loadBlog.social_image} alt="Keyboard" /> 
+      <h3 className="blogtitle">{loadBlog.title}</h3>
+      <h4>Published: {loadBlog.readable_publish_date}</h4>
+      <p>{loadBlog.description}</p>
+      <h4 className="taglist">{loadBlog.tag_list}</h4>
+      <h4><a className="myblogcard_button" href={loadBlog.canonical_url} target="_blank">Read</a></h4>
       
       {/* TODO Add Tags  */}
     </div>   
