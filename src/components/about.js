ababccf71148a04802c31bf6dd88e0dd0ee86f99
M5FGN <m5fgn@outlook.com>
2022-03-05 09:53:13 +0000

    Test auto deploy

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ function About() {
       <a href="#" className="link" name="about"></a> 
       <h2>About</h2>
 <p className="about_text">      Having worked in IT Training and Support roles for many years I was made redundant in April 2020 and with the job market stagnated due the lockdown I turned to coding to pass the time. Coding had been a hobby for many years and I had been lucky enough to be able to incorporate it in someway into most of my roles until then. Like many during the lockdowns I began considering the shape of my future life and I decided to do more of the things I loved. Coding being one of those things. I applied and was accepted to the 16 week intensive Code Clan Software Development course where I learned Python, React and Java. Soon after finishing the course I began working as a Junior Software Developer mainly using JavaScript and SQL.</p>
-    <p className="about_text">  Outside work I am continuing to learn new coding skills and I am also pursuing my other love which is history. I spend my free time visiting and researching aspects of Scottish and British History and intend to start documenting this on a new blog soon.</p>
+    <p className="about_text">  Outside work I am continuing to learn new coding skills and I am also pursuing my other love which is history. I spend my free time visiting and researching aspects of Scottish and British History and intend to start documenting this on a new blog soon. </p>
     </div>    
   );                          
 }                           
