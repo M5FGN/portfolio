46d61670c22d16592a1c596eeb2fd9b3cb494d11
M5FGN <m5fgn@outlook.com>
2022-05-09 16:14:55 +0100

    Add draft faq component

---
+++ src/components/faq.js
@@ -0,0 +1,22 @@
+import React from 'react';    
+     
+
+function faq() {              
+  
+  return (                    
+    //  TODO - component to be added to the home page.
+     <div className="faq">
+     <h2>FAQ</h2> 
+    <p>What is Codeclan?</p>
+    <p>Codeclan is an intensive 16 week bootcamp with over 800 hours if code time made up of lessons, labs, homework and projects. During the course I learned:-</p>
+    {/* TODO List the skills I learned */}
+    <p>DO you have a Computer Science Degree?</p>
+    <p>Yes, I graduated in 2016 with a BA Computer Science. During the course I learned:-</p>
+        {/* TODO List the skills I learned */}
+        <p>Why did you do a coding Bootcamp if you have a Computer Science degree</p>
+        <p>I have worked in IT for over 18 years and many of the modules of the degree were relevant to this area of my work and at the time whilst I enjoyed the code related modules a career as a Software Developer was not my focus. During the Covid pandemic after considering my future I decided to change careers and persue a Software Development career. The Codeclan bootcamp helped me to update my skills in this area.</p>
+    {/* TODO Rewording needed */}
+ </div>
+  );                          
+}                           
+export default faq;  
--- src/components/myskills.js
+++ src/components/myskills.js
@@ -11,20 +11,26 @@ function MySkills() {
          <img className='shields' src='https://img.shields.io/badge/-HTML-5c0f84?style=for-the-badge&logo=html5&logoColor=white' alt="Logo"/> 
          <img className='shields' src='https://img.shields.io/badge/-CSS-5c0f84?style=for-the-badge&logo=CSS3&logoColor=white' alt="Logo"/> 
          <img className='shields' src='https://img.shields.io/badge/-JavaScript-5c0f84?style=for-the-badge&logo=JavaScript&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-Python-5c0f84?style=for-the-badge&logo=Python&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-React-5c0f84?style=for-the-badge&logo=React&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-Node.js-5c0f84?style=for-the-badge&logo=nodedotjs&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-Express.js-5c0f84?style=for-the-badge&logo=Express.js&logoColor=white' alt="Logo"/> <br/>
+         <img className='shields' src='https://img.shields.io/badge/-Python-5c0f84?style=for-the-badge&logo=Python&logoColor=white' alt="Logo"/>
+         <img className='shields' src='https://img.shields.io/badge/-Flask-5c0f84?style=for-the-badge&logo=Flask&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-Java-5c0f84?style=for-the-badge&logo=Java&logoColor=white' alt="Logo"/>
+         <img className='shields' src='https://img.shields.io/badge/-Spring-5c0f84?style=for-the-badge&logo=Spring&logoColor=white' alt="Logo"/> <br/>
+
+         <img className='shields' src='https://img.shields.io/badge/-PostgreSQL-5c0f84?style=for-the-badge&logo=PostgreSQL&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-microsoft%20sql%20server-5c0f84?style=for-the-badge&logo=microsoftsqlserver&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-mysql-5c0f84?style=for-the-badge&logo=mysql&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-mongodb-5c0f84?style=for-the-badge&logo=mongodb&logoColor=white' alt="Logo"/><br/>
+
+         <img className='shields' src='https://img.shields.io/badge/-cypress-5c0f84?style=for-the-badge&logo=cypress&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-mocha-5c0f84?style=for-the-badge&logo=mocha&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-chai-5c0f84?style=for-the-badge&logo=chai&logoColor=white' alt="Logo"/> <br/>
+
          <img className='shields' src='https://img.shields.io/badge/-Git-5c0f84?style=for-the-badge&logo=Git&logoColor=white' alt="Logo"/> 
          <img className='shields' src='https://img.shields.io/badge/-GitHub-5c0f84?style=for-the-badge&logo=GitHub&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-VS%20Code-5c0f84?style=for-the-badge&logo=visual-studio-code&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-PostgreSQL-5c0f84?style=for-the-badge&logo=PostgreSQL&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-Flask-5c0f84?style=for-the-badge&logo=Flask&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-Java-5c0f84?style=for-the-badge&logo=Java&logoColor=white' alt="Logo"/> <br/>
-         <img className='shields' src='https://img.shields.io/badge/-Express.js-5c0f84?style=for-the-badge&logo=Express.js&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-React-5c0f84?style=for-the-badge&logo=React&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-Spring-5c0f84?style=for-the-badge&logo=Spring&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-microsoft%20sql%20server-5c0f84?style=for-the-badge&logo=microsoftsqlserver&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-Node.js-5c0f84?style=for-the-badge&logo=nodedotjs&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-mongodb-5c0f84?style=for-the-badge&logo=mongodb&logoColor=white' alt="Logo"/> 
-         <img className='shields' src='https://img.shields.io/badge/-cypress-5c0f84?style=for-the-badge&logo=cypress&logoColor=white' alt="Logo"/> 
+         <img className='shields' src='https://img.shields.io/badge/-VS%20Code-5c0f84?style=for-the-badge&logo=visual-studio-code&logoColor=white' alt="Logo"/>
 
      </div>
  
