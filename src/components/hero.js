f7fd54aeb6c636fd796cd0302299377e4ec6ee45
Mary <33698169+M5FGN@users.noreply.github.com>
2022-09-22 09:32:58 +0100

    Update Text

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,6 @@
 import React from 'react';    
 import hero_img from '../assets/hero_img.png'
-
-     
-
+import heart from '../assets/heart.png'
 
 function Hero() {              
   
@@ -10,7 +8,7 @@ function Hero() {
       <div className='hero'>  
          <div className="hero-left"><h1 className="hero_header">Eat, Sleep, Code, Repeat</h1>
          <p className="hero_text">Hi, my name is Mary. Welcome to my Portfolio.</p>
-         <p className= "center">*****</p>
+         <p className= "center"><img className="heart" src={heart} alt="heart" /><img className="heart" src={heart} alt="heart" /><img className="heart" src={heart} alt="heart" /></p>
          <p className="hero_text">Turning coding from a hobby into a career during the pandemic. </p>
          <p className="hero_text">Code Clan Graduate now working as a Junior Software Developer.</p>
          </div>
